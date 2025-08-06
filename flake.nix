18ff0a12e761a2542f493087e457ec43200e6073
Sojus07 <Sojus07>
2025-08-06 22:53:35 +0200

    kekl

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self }: 
+  outputs = { self, nixpkgs }: 
   {
     nixosModules.default = import ./default.nix;
   };
